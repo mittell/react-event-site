682f89b5d5a7c13de90668d8bf48ceb3f94e4c82
Chris Mittell <mittell@hotmail.co.uk>
2022-08-29 11:41:46 +0900

    feat: added base route setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import GlobalStyle from './globalStyles';
+import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 function App() {
-	return <div></div>;
+	return (
+		<Router>
+			<GlobalStyle />
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route path='/sign-up' component={SignupPage} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
