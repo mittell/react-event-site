a911f649da225304944136843b3f3c0253d8532e
Chris Mittell <mittell@hotmail.co.uk>
2022-08-29 13:34:29 +0900

    feat: added Info component template and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
+import Info from '../components/Info/Info';
 
 function HomePage() {
-	return <Hero>HomePage</Hero>;
+	return (
+		<>
+			<Hero />
+			<Info id='about' />
+		</>
+	);
 }
 
 export default HomePage;
