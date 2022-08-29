1b834f111230b20f5544595155147f1127b1216f
Chris Mittell <mittell@hotmail.co.uk>
2022-08-29 14:44:47 +0900

    feat: added Content component template and styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Content from '../components/Content/Content';
 import Features from '../components/Features/Features';
 import Hero from '../components/Hero/Hero';
 import Info from '../components/Info/Info';
+import { heroData } from '../data/HeroData';
 
 function HomePage() {
 	return (
@@ -9,6 +11,11 @@ function HomePage() {
 			<Hero />
 			<Info id='about' />
 			<Features id='features' />
+			<div id='events'>
+				{heroData.map((contentData, index) => (
+					<Content {...contentData} key={index} />
+				))}
+			</div>
 		</>
 	);
 }
